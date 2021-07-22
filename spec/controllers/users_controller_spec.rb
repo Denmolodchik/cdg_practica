365e5ebaf6ab4af095f038e31fe8ac61f9d8faa6
DESKTOP-FD4EURD\denmo <den.molodchik@outlook.com>
2021-07-22 21:56:44 +0300

    Change tests for controllers: users.

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,48 @@
+#frozen_string_literal: true
+
+require 'rails_helper'
+require 'capybara/rails'
+
+RSpec.describe UsersController, type: :controller do
+    let(:user) { create :user }
+    let(:params) { { id: user } }
+
+    before { sign_in user }
+
+    describe '#index' do
+        subject { get :index, params: params }
+
+        it 'assigns @users' do
+            subject
+            expect(assigns(:users)).to eq([user])
+        end
+
+        it { is_expected.to render_template('index') }
+    end
+
+    describe '#show' do
+        let(:params) { { id: user.id } }
+        subject { get :show, params: params }
+
+        it 'assigns @user' do
+            subject
+            expect(assigns(:user)).to eq(user)
+        end
+
+        it { is_expected.to render_template(:show) }
+
+        context 'when a user is watching another user' do
+            let(:params) { { id: user1.id } }
+            let!(:user1) { create :user }
+
+            subject { get :show, params: params }
+
+            it 'assigns @user' do
+                subject
+                expect(assigns(:user)).to eq(user1)
+            end
+        end
+
+    end
+    
+end
